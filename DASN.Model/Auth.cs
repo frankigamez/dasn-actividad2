f5ecbae65c1b2b52689d09a3bc10afe469621cc2
fran <fran@sportmaniacs.com>
2017-10-30 13:21:37 +0000

    (se quedó sin marcar del anterior commit)

--- DASN.Model/Auth.cs
+++ DASN.Model/Auth.cs
@@ -1,29 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
-namespace DASN.Model
+namespace DASN.DataModel
 {
-    public class Post
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public bool IsPublic { get; set; }
-        public int UserId { get; set; }
-        public DateTime PublishedAt { get; set; }
-    }
-
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime CreatedAt { get; set; }
-
-        public ICollection<Auth> Auths { get; set; }
-        public ICollection<Post> Posts { get; set; }
-    }
-
     public class Auth
     {
         public int UserId { get; set; }
