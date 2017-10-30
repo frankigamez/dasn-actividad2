f9091c2c540ebfeef7a22d4a89e6dcb9086f7c10
fran <fran@sportmaniacs.com>
2017-10-30 12:57:30 +0000

    Creados los dos proyectos que soportaran la persistencia: - DASN.DataModel -> define los modelos - DASN.DataContext -> define la interacción con la capa de persistencia

---
+++ DASN.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DASN.Model")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5936ef45-f56e-4bf0-b65d-98fa47de6b9a")]
