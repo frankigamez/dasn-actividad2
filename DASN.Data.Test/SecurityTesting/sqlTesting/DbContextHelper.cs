ef2542036e7e11cf941c353b1e2f2a09fb1a8355
Francisco Gámez <franciscog.gamez@ticntap.com>
2017-11-01 00:06:51 +0000

    Utilidad para configurar el contexto de Entity Framework, para hacer testing

---
+++ DASN.Data.Test/SecurityTesting/sqlTesting/DbContextHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+
+namespace DASN.Data.Test.SecurityTesting.sqlTesting
+{
+    internal class DbContextHelper
+    {
+        private const string Dbfile = "test.db";
+
+        public static void StartContext()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings(
+                name: "DASNDBTest",
+                connectionString: $"Data Source={Dbfile};Version=3;New=True;",
+                providerName: "System.Data.SQLite"));
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("connectionStrings");            
+        }
+
+        public static void EndContext()
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Remove(name: "DASNDBTest");
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("connectionStrings");
+        }
+    }
+}
