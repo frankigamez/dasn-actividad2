ee432dd5ea0671ebc46ed0157774b7250fb67c0e
Francisco Gámez <franciscog.gamez@ticntap.com>
2017-11-01 00:01:41 +0000

    Modelo de datos previsto

---
+++ DASN.Data/Model/Auth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DASN.Data.Model
+{
+    public class Auth
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }       
+        public string Hash { get; set; }
+        public string Salt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ DASN.Data/Model/Post.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DASN.Data.Model
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsPublic { get; set; }        
+        public DateTime CreatedAt { get; set; }        
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
