4fb43b23a0c7a66c7f867cb67cca2b98330c71ac
Francisco Gámez <franciscog.gamez@ticntap.com>
2017-11-01 00:03:38 +0000

    Servicios para interactuar con los datos

---
+++ DASN.Data/Service/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using DASN.Data.Model;
+
+namespace DASN.Data.Service
+{
+    public class UserService
+    {
+        private readonly IContext _context;
+
+        public UserService(IContext context)
+        {
+            _context = context;
+        }
+
+        public User GetUserById(int userId) => _context.Users
+            .FirstOrDefault(x => x.Id == userId);
+
+        public User AddUser(string email, DateTime createdAt)
+        {
+            var entity = _context.Users.Add(new User
+            {
+                Email = email,
+                CreatedAt = createdAt
+            });
+
+            _context.SaveChanges();
+
+            return entity;
+        }
+    }
+}
